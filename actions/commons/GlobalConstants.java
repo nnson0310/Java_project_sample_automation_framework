71a7db051c969813c21137acd794beb8111864ff
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-06-03 10:28:07 +0700

    Attach screen short to ReportNG and ExtentReport html

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -11,6 +11,12 @@ public class GlobalConstants {
 
     public static final int threadSleepMillis = 1000;
 
-    public static final String uploadFileLocation = System.getProperty("user.dir") + File.separator + "uploadFiles" + File.separator;
+    public static final String projectPath = System.getProperty("user.dir");
+
+    public static final String uploadFileLocation = projectPath + File.separator + "uploadFiles" + File.separator;
+
+    public static final String reportNGCapturedScreenshot = projectPath + File.separator + "reportScreenshot" + File.separator + "reportNG" + File.separator;
+
+    public static final String extentReportCapturedScreenshot = projectPath + File.separator + "reportScreenshot" + File.separator + "extentReport" + File.separator;
 
 }
